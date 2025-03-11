72caa31d81d86e46262ef456bd8efda0ab3205a6
prajwal434 <prajwal.anand434@gmail.com>
2025-03-11 11:35:13 +0530

    functions put into modules

---
+++ Todo-list/Functions.py
@@ -0,0 +1,10 @@
+def get_todos(filepath = "todos.txt"):
+    """opens the file and reads the existing todos"""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath = "todos.txt"):
+    """write the todos in the text file"""
+    with open(filepath,'w') as file:
+        file.writelines(todos_arg)
--- Todo-list/main.py
+++ Todo-list/main.py
@@ -1,9 +1,4 @@
-def get_todos():
-    with open('todo.txt', 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
+import Functions
 
 while True:
     User_action = input("type add, show, edit, complete or exit: ")
@@ -13,17 +8,16 @@ while True:
     if User_action.startswith("add"):
           todo = User_action[4:]
 
-          todos = get_todos()
+          todos = Functions.get_todos()
 
 
           todos.append(todo + '\n') # This appends the input correctly
 
-          with open("todo.txt","w") as file:
-               file.writelines(todos)
+          Functions.write_todos(todos)
 
 
     elif User_action.startswith("show"):
-          todos = get_todos()
+          todos = FUnctions.get_todos()
 
 
 
@@ -42,15 +36,14 @@ while True:
             number = number - 1
             print(number)
             
-            todos = get_todos()
+            todos = Functions.get_todos()
 
           
             
             new_todo = input("enter new todo:")
             todos[number] = new_todo + '\n'
 
-            with open("todo.txt", "w") as file:
-              file.writelines(todos)
+            Functions.write_todos(todos)
           except ValueError:
               print("your command is not valid")
               continue
@@ -68,7 +61,7 @@ while True:
 
             
 
-            todos = get_todos()
+            todos = Functions.get_todos()
 
 
 
@@ -76,10 +69,7 @@ while True:
             Todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            
-
-            with open("todo.txt", "w") as file:
-              file.writelines(todos)
+            Functions.write_todos(todos)
           
             message = f"todos {Todo_to_remove} is removed from the todo-list"
               
