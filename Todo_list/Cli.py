1f9197d76512efa553fae07aa9f150c38204ca92
prajwal434 <prajwal.anand434@gmail.com>
2025-03-26 11:10:28 +0530

    created standalone executable file

---
+++ Todo_list/Cli.py
@@ -0,0 +1,93 @@
+import Functions
+import time
+
+now = time.strftime("%b %d, %Y  %H:%M:%S")
+print("It is", now)
+
+
+while True:
+    User_action = input("type add, show, edit, complete or exit: ")
+    User_action = User_action.strip()
+
+    
+    if User_action.startswith("add"):
+          todo = User_action[4:]
+
+          todos = Functions.get_todos()
+
+
+          todos.append(todo + '\n') # This appends the input correctly
+
+          Functions.write_todos(todos)
+
+
+    elif User_action.startswith("show"):
+          todos = Functions.get_todos()
+
+
+
+          for index,item in enumerate(todos):  # Iterate through all items in the list
+                row = f"{index + 1}.{item}"
+                row = row.strip("\n")
+                print(row)
+
+
+    elif User_action.startswith("edit"):
+          try:
+
+
+            number = int(User_action[4:])
+
+            number = number - 1
+            print(number)
+            
+            todos = Functions.get_todos()
+
+          
+            
+            new_todo = input("enter new todo:")
+            todos[number] = new_todo + '\n'
+
+            Functions.write_todos(todos)
+          except ValueError:
+              print("your command is not valid")
+              continue
+
+
+
+
+
+
+    elif  User_action.startswith("complete"):
+        try:
+            number = int(User_action[9:])
+            index = number-1
+
+
+            
+
+            todos = Functions.get_todos()
+
+
+
+            
+            Todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            Functions.write_todos(todos)
+          
+            message = f"todos {Todo_to_remove} is removed from the todo-list"
+              
+            print(message)
+        except IndexError:
+            print("There is no item with that number.")
+            continue
+
+
+
+    elif  User_action.startswith("exit"):
+            break
+    else:
+         print("command is not valid")
+
+print("bye!")
---
+++ Todo_list/gui.py
@@ -0,0 +1,78 @@
+import  Functions
+import FreeSimpleGUI as sg
+import time
+import os
+
+if not os.path.exists("todo.txt"):
+   with open("todo.txt", "w") as file:
+       pass
+
+sg.theme("black")
+clock = sg.Text('',key='clock')
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
+add_button = sg.Button("Add")
+list_box = sg.Listbox(values=Functions.get_todos(),key="todos",
+                     enable_events=True, size=[45,10])
+edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+Exit_button = sg.Button("Exit")
+
+
+
+window = sg.Window("My To-Do App",
+                   layout=[[clock],
+                           [label],
+                           [input_box, add_button],
+                           [list_box,edit_button,complete_button],
+                           [Exit_button]],
+                   font=('Helvetica',12))
+
+while True:
+    event,values = window.read(timeout=10)
+    if event in (sg.WIN_CLOSED, "Exit"):
+        break
+    window["clock"].update(value=time.strftime("%b %d, %Y  %H:%M:%S"))
+    print(event)
+    print(values)
+
+    match event:
+        case "Add":
+            todos = Functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            Functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            try:
+             todo_to_edit = values['todos'][0]
+             new_todo = values['todo']
+
+             todos = Functions.get_todos()
+             index = todos.index(todo_to_edit)
+             todos[index] = new_todo
+             Functions.write_todos(todos)
+             window['todos'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select an item to edit", font=("helvetica",20),text_color="white")
+        case "Complete":
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = Functions.get_todos()
+                todos.remove(todo_to_complete)
+                Functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value="")
+            except IndexError:
+                sg.popup("please select an item to delete", font=('helvetica',20))
+        case "Exit":
+            break
+
+        case "todos":
+            window['todo'].update(value=values["todos"][0])
+
+        case sg.WIN_CLOSED:
+            break
+window.close()
+
